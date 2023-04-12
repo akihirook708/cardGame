875e6758acc0bbd23372a9656de5b0f8d149114c
akihirook708 <akihirook708@gmail.com>
2023-04-13 08:25:58 +0900

    add porker

--- assets/js/app copy.js
+++ assets/js/app copy.js
@@ -1,4 +1,10 @@
 (() => {
+    // 利用するカード番号の配列を作成、今回は１〜１３全て
+    const numbers = [...Array(13)].map((_, i) => i+1);
+
+    // s : スペード, d : ダイヤ, h : ハート, c : クローバー
+    const suits = ['s', 'd', 'h', 'c'];
+
     // カードクラス
     class Card {
         constructor(suit, num) {
@@ -9,55 +15,29 @@
         }
     }
 
-    // シャッフル済みカードの山のクラス
-    class Deck {
-        constructor(suits,numbers,joker){
-            this.array = [];
-            // カード情報を配列に格納
-            for (let i = 0; i < suits.length; i++) {
-                for (let j = 0; j < numbers.length; j++) {
-                    const card = new Card(suits[i], numbers[j]);
-                    this.array.push(card);
-                }
-            }
-            if(joker>0){
-                for(let k = 0;k < joker; k++){
-                    const card = new Card('j',99);
-                    this.array.push(card);
-                }
-            }
-            this.array = this.shuffle(this.array);
+    // 全てのカード情報を入れる配列を宣言
+    const cards = [];
+    // カード情報を配列に格納
+    for (let i = 0; i < suits.length; i++) {
+        for (let j = 1; j <= 13; j++) {
+            const card = new Card(suits[i], j);
+            cards.push(card);
         }
-        // シャッフルする関数
-        shuffle = (arrays) => {
-            const array = arrays.slice();
-            for (let i = array.length - 1; i >= 0; i--) {
-                const randomIndex = Math.floor(Math.random() * (i + 1));
-                [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
-            }
-            return array;
-        };
-        remain = () => {
-            return this.array.length;
-        };
-        deal = (num) => {
-            const cardArray = this.array.splice(0,num);
-            return cardArray;
-        };
-    };
-
-    // 利用するカード番号の配列を作成、今回は１〜１３全て
-    const numbers = [...Array(13)].map((_, i) => i+1);
-
-    // s : スペード, d : ダイヤ, h : ハート, c : クローバー
-    const suits = ['s', 'd', 'h', 'c'];
-
-    // 神経衰弱用のカードを作成
-    const deck = new Deck(suits,numbers,0);
+    }
 
     // 残カード数,操作カウンターの変数設定
-    let remain = deck.remain();
+    let remain = cards.length;
     let counter = 0;
+
+    // シャッフルする関数
+    const shuffle = (arrays) => {
+        const array = arrays.slice();
+        for (let i = array.length - 1; i >= 0; i--) {
+            const randomIndex = Math.floor(Math.random() * (i + 1));
+            [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+        }
+        return array;
+    };
 
     // 1枚目のカードの変数を宣言 最初はnull
     let firstCard = null;
@@ -66,7 +46,7 @@
 
     // カードをめくった時の処理
     const flip = (e) => {
-        const flippedCard = e.target;
+        let flippedCard = e.target;
         // カードにクラスbackがついていないか、flipTimerIdが動作しているとき
         if (!flippedCard.classList.contains('back') || flipTimerId) {
             return; //表のカードをクリックしても何も反応させない。
@@ -101,18 +81,18 @@
         }
     };
 
-    // HTML生成
-    const table = document.querySelector('.cardTable');
-    const setCard = () => {
-        const cardArray = deck.deal(52);
+    // シャッフルし、HTML生成
+    const table = document.querySelector('.table');
+    const shuffleCard = () => {
+        const shuffled = shuffle(cards);
         for (let i = 0; i < suits.length; i++) {
             const tr = document.createElement('tr');
             table.appendChild(tr);
             for (let j = 0; j < 13; j++) {
                 const td = document.createElement('td');
-                td.num = cardArray[i*13+j].num;
+                td.num = shuffled[i*13+j].num;
                 td.style.backgroundImage = `url(./assets/cards_svg/${
-                    cardArray[i * 13 + j].front
+                    shuffled[i * 13 + j].front
                 })`;
                 td.classList.add('card', 'back');
                 td.onclick = flip; // カードをクリックしたときの関数
@@ -120,5 +100,5 @@
             }
         }
     };
-    setCard();
+    shuffleCard();
 })();
